fedca69b1b5bd772daa5ca3446ec40ca2f22cec6
Amine <mohamedamine.naffeti@gmail.com>
2024-05-15 12:10:48 +0100

    small update

--- actes-web-app/src/acts/ActValidation.tsx
+++ actes-web-app/src/acts/ActValidation.tsx
@@ -1,13 +1,14 @@
 import React, { ReactNode, useState } from "react";
-import { useTypedSelector } from "../store/index.ts";
-import { ActeType, TermAgreement } from "../data/interfaces.ts";
+import { useAddActMutation, useTypedSelector } from "../store/index.ts";
+import { ActAddress, ActeType, AddAct, BirthInfo, DeathInfo, MarriageInfo, TermAgreement } from "../data/interfaces.ts";
 import { getBirthActValidationsComponent, getDeathActValidationsComponent, getMarriageActValidationsComponent } from "../data/helpers.tsx";
 import Button from "../../src/components/common/Button.tsx"
 import CheckBox from "../components/common/CheckBox.tsx";
 import { FormikHelpers, useFormik } from "formik";
 import { termAgreementSchema } from "../data/validations.tsx";
 import PaiementMethod from "../components/actes/PaiementMethod.tsx";
-
+import { useSnackbar } from 'react-simple-snackbar'
+import { useNavigate } from "react-router-dom";
 interface ActValidationProps {
   setActiveStep: (step: number) => void;
 }
@@ -15,6 +16,48 @@ interface ActValidationProps {
 const ActValidation: React.FC<ActValidationProps> = ({ setActiveStep }) => {
 
   const [openModal, setOpenModal] = useState<boolean>(false)
+
+  const [addAct] = useAddActMutation();
+  const [openSnackbar] = useSnackbar()
+  const navigate = useNavigate()
+
+  const successSnackBarContent: ReactNode = (
+    <div className="underline decoration-green-600">
+      Demande enregistrée avec succès
+    </div>
+  )
+
+  const failureSnackBarContent: ReactNode = (
+    <div className="underline decoration-red-700">
+      une erreur s'est produite
+    </div>
+  )
+
+  const { marriageInfo, birthInfo, deathInfo, actAddressInfo } = useTypedSelector(
+    (state) => ({ 
+      marriageInfo: state.marriageAct, 
+      birthInfo: state.birthAct, 
+      deathInfo: state.deathAct, 
+      actAddressInfo: state.actAddress, 
+      actType: state.auth.actType 
+    })
+  )
+
+  const handleAddAct = async () => {
+    if (actType && actAddressInfo) {
+      let requestBody: AddAct = { actType, actAddressInfo: actAddressInfo as Required<ActAddress> }
+      if (actType === ActeType.BIRTH) requestBody.birthInfo = birthInfo as Required<BirthInfo>
+      else if (actType === ActeType.DEATH) requestBody.deathInfo = deathInfo as Required<DeathInfo>
+      else if (actType === ActeType.MARRIAGE) requestBody.marriageInfo = marriageInfo as Required<MarriageInfo>
+      try {
+        await addAct(requestBody).unwrap()
+        openSnackbar(successSnackBarContent, 2500)
+      } catch (error) {
+        openSnackbar(failureSnackBarContent, 2500)
+      }
+      
+    }
+  }
 
   const actType: ActeType | undefined = useTypedSelector((state) => state.auth.actType)
   const { birthAct, marriageAct, deathAct, actAddress} = useTypedSelector(({ birthAct, marriageAct, deathAct, actAddress }) => ({ birthAct, marriageAct, deathAct, actAddress }))
@@ -30,7 +73,9 @@ const ActValidation: React.FC<ActValidationProps> = ({ setActiveStep }) => {
 
   const handleSubmitForm = async (values: TermAgreement, actions: FormikHelpers<TermAgreement>) => {
     // const validValues = values as Required<TermAgreement>
-    setOpenModal(true)
+    //  setOpenModal(true)
+    await handleAddAct()
+    navigate('/');
   }
 
   const { values, errors, isSubmitting, handleSubmit, setFieldValue } = useFormik<TermAgreement>({
